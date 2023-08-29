1af811e8bc51f90f5bc834e1c81c5c56187e7be8
Wellington <wellingtonmachioni@gmail.com>
2023-08-29 17:03:50 -0300

    refactor: removido componente Main e importado Form direto no App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/title/';
-import Main from './src/components/Main/';
+import Form from './src/components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
-      <Main />
+      <Form />
     </View>
   );
 }
