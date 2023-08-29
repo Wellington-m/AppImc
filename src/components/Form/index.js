b17968a3b6f01a4be1e110ec590b05f761011042
Wellington <wellingtonmachioni@gmail.com>
2023-08-29 17:57:07 -0300

    refactor: estilizado Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { TextInput, View, Text, Button } from 'react-native';
+import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 import ResultImc from './ResultImc';
+import styles from './style';
 
 export default function Form(){
 
@@ -29,27 +30,37 @@ export default function Form(){
   }
 
   return(
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
           placeholder='Ex: 1.97'
           keyboardType='numeric'
         />
 
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder='Ex: 80.9'
           keyboardType='numeric'
         />
-        <Button
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => {
+            validationImc()
+          }}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
+        {/* <Button
           title={textButton}
           onPress={() => validationImc()}
-        />
+        /> */}
       </View>
       <ResultImc resultImc={imc} messageResultImc={messageImc} />
     </View>
